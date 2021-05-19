41ed26e9102cc6c26ffc1dfa154adc7713fdeae8
JoJaCo <contrerasjorge226@gmail.com>
2021-05-19 15:55:20 -0700

    working on xml layout

--- app/src/main/java/com/jorge/contreras/xmltojson/data/XmlParser.kt
+++ app/src/main/java/com/jorge/contreras/xmltojson/data/XmlParser.kt
@@ -1,4 +1,12 @@
 package com.jorge.contreras.xmltojson.data
 
+import org.xmlpull.v1.XmlPullParserException
+import java.io.IOException
+import kotlin.jvm.Throws
+
 class XmlParser {
+    @Throws(XmlPullParserException::class, IOException::class)
+    fun parse( ){
+
+    }
 }
