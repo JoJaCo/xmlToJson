98db539e3d20a59c65b2d867cf413351094a3aa7
JoJaCo <contrerasjorge226@gmail.com>
2021-05-19 15:29:08 -0700

    starting on prasing xml file

---
+++ app/src/main/java/com/jorge/contreras/xmltojson/data/XmlParser.kt
@@ -0,0 +1,4 @@
+package com.jorge.contreras.xmltojson.data
+
+class XmlParser {
+}
--- app/src/main/java/com/jorge/contreras/xmltojson/data/addressX.kt
+++
@@ -1,5 +0,0 @@
-package com.jorge.contreras.xmltojson.data
-
-data class addressX(
-    val AddressBook: AddressBook
-)
